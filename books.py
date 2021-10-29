748eb7890882be75c5da5bb90cd23f42fb2b8b7f
PaulinaFigol <43251266+PaulinaFigol@users.noreply.github.com>
2021-10-29 15:54:34 +0100

    Update books.py

--- books.py
+++ books.py
@@ -212,7 +212,7 @@ with row3_2:
 data2 = pd.DataFrame({'address': [user_input], 
         'lat': [data_postcode['lat'].mean()], 
         'lgt':[data_postcode['lgt'].mean()]})
-figD = px.scatter_mapbox(data2, lat="lat", lon="lgt", hover_name="address", color_discrete_sequence=["fuchsia"], zoom=6)
+figD = px.scatter_mapbox(data2, lat="lat", lon="lgt", hover_name="address", color_discrete_sequence=["fuchsia"], zoom=8)
 figD.update_layout(mapbox_style="open-street-map")
 figD.update_layout(margin={"r":0,"t":0,"l":0,"b":0}) 
 st.plotly_chart(figD)
