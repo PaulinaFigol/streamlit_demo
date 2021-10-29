238769b07c895ccad6098d1c73c166aab67eb7e2
PaulinaFigol <43251266+PaulinaFigol@users.noreply.github.com>
2021-10-29 22:13:08 +0100

    Update books.py

--- books.py
+++ books.py
@@ -224,10 +224,10 @@ def filter_data(data_filtered, user_input_bedrooms, user_input_property):
     if user_input_property != None:
         data_filtered = data_filtered[data_filtered['propertyType'] == user_input_property]
     
-    data_filtered['lat_new'] = data_filtered['lat']+ np.random.normal(loc=0.0, scale=0.00004, size=len(data)) 
-    data_filtered['lgt_new'] = data_filtered['lgt']+ np.random.normal(loc=0.0, scale=0.00004, size=len(data))
+    data_filtered['lat_new'] = data_filtered['lat']+ np.random.normal(loc=0.0, scale=0.00004, size=len(data_filtered)) 
+    data_filtered['lgt_new'] = data_filtered['lgt']+ np.random.normal(loc=0.0, scale=0.00004, size=len(data_filtered))
     
-    fig = px.scatter_mapbox(data, lat="lat_new", lon="lgt_new", hover_name="address", color_discrete_sequence=["fuchsia"], zoom=12)
+    fig = px.scatter_mapbox(data_filtered, lat="lat_new", lon="lgt_new", hover_name="address", color_discrete_sequence=["fuchsia"], zoom=12)
     fig.update_layout(mapbox_style="open-street-map")
     fig.update_layout(margin={"r":0,"t":0,"l":0,"b":0})
     st.plotly_chart(fig)
