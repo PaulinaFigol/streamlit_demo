53adc459ed65c83c332d992c411967067fe17e82
PaulinaFigol <43251266+PaulinaFigol@users.noreply.github.com>
2021-10-30 21:29:41 +0100

    Update books.py

--- books.py
+++ books.py
@@ -242,7 +242,7 @@ st.write("Choose features on the left to filter by number of bedrooms and proper
 data_postcode['transactions_price_numeric'] = data_postcode['transactions_price'].apply(lambda x: pd.to_numeric(re.sub('[^A-Za-z0-9]+', '',  x)))
 mean_price = data_postcode['transactions_price_numeric'].mean()
 no_properties = len(data_postcode['address'].unique())
-md_results = f"The average price for properties in this postcode is £**{mean_price:.2f}**there are **{no_properties:,}** properties."
+md_results = f"The average price for properties in this postcode is £**{mean_price:.2f,}** and there are **{no_properties:,}** properties."
 st.markdown(md_results)
     
 if user_input_bedrooms == None and user_input_property == None:
