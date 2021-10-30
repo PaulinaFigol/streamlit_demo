a2c902c2517ecd6c8bc479f752eb4b2031bef2ce
PaulinaFigol <43251266+PaulinaFigol@users.noreply.github.com>
2021-10-31 00:11:20 +0100

    Update books.py

--- books.py
+++ books.py
@@ -61,20 +61,22 @@ with st.sidebar:
                                     [None, 2015, 2016, 2017, 2018, 2019, 2020, 2021])
     st.write("*Starting year from which the latest transaction will be shown (until now)")
     st.write("")
-    st.subheader("2. Choose Features")
-    st.write("")
-    st.write("Below you can choose the desired number of bedrooms and/or property type.")
     
-    user_input_bedrooms = st.sidebar.selectbox('Choose the number of bedrooms:',
-                                    [None, 0, 1, 2, 3, 4, 5, 6, 7, 8])
-    st.write("The Bedroom distribution bar chart shows count of bedrooms by property type.") 
-    st.write("")
-    user_input_property = st.sidebar.selectbox('Choose property type:',
-                                    [None, 'Detached', 'Flat', 'Semi-Detached', 'Terraced'])
-    st.write("The Property Type pie chart shows a proportion of property types.")
-    st.write("")
-    st.subheader("Hover over the plots to see details or to zoom in.")
-    st.write("")
+    if user_input != '' and user_input_year != None:
+        st.subheader("2. Choose Features")
+        st.write("")
+        st.write("Below you can choose the desired number of bedrooms and/or property type.")
+        
+        user_input_bedrooms = st.sidebar.selectbox('Choose the number of bedrooms:',
+                                        [None, 0, 1, 2, 3, 4, 5, 6, 7, 8])
+        st.write("The Bedroom distribution bar chart shows count of bedrooms by property type.") 
+        st.write("")
+        user_input_property = st.sidebar.selectbox('Choose property type:',
+                                        [None, 'Detached', 'Flat', 'Semi-Detached', 'Terraced'])
+        st.write("The Property Type pie chart shows a proportion of property types.")
+        st.write("")
+        st.subheader("Hover over the plots to see details or to zoom in.")
+        st.write("")
 
 if user_input == '' or user_input_year == None:
     st.stop()
