1bcd76bd0ec4f6c8f1ff4ea78774484be76c16f6
PaulinaFigol <43251266+PaulinaFigol@users.noreply.github.com>
2021-10-30 21:17:41 +0100

    Update books.py

--- books.py
+++ books.py
@@ -239,9 +239,10 @@ def filter_data(data_filtered, user_input_bedrooms, user_input_property):
 st.write('')
 st.subheader("View Properties on Map")
 st.write("Choose features on the left to filter by number of bedrooms and property type.")
+data_postcode['transactions_price'] = data_postcode['transactions_price'].apply(lambda x: pd.to_numeric(re.sub('[^A-Za-z0-9]+', '',  x)))
 mean_price = data_postcode['transactions_price'].mean()
 no_properties = len(data_postcode['address'].unique())
-md_results = f"The average price for properties in this postcode is **{mean_price:.2f}**there are **{no_properties:,}** properties."
+md_results = f"The average price for properties in this postcode is £**{mean_price:.2f}**there are **{no_properties:,}** properties."
 st.markdown(md_results)
     
 if user_input_bedrooms == None and user_input_property == None:
